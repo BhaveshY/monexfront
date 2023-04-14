83ffd93e4959517998b3f8c861a0fd03f1484a3f
Bhavesh Yeole <byeole80@gmail.com>
2023-04-14 15:49:53 +0530

    added charts

--- screens/Analytics.js
+++ screens/Analytics.js
@@ -1,25 +1,82 @@
-import { AddIcon, Text, Fab, Box, Skeleton, HStack, Button, ChevronLeftIcon } from "native-base";
+import React from 'react';
+import {
+  AddIcon,
+  Text,
+  Fab,
+  Box,
+  Skeleton,
+  HStack,
+  Button,
+  ChevronLeftIcon,
+  ScrollView,
+} from 'native-base';
 import TransactionListNew from '../components/TransactionList';
-import { useCallback, useState } from "react";
-import { useFocusEffect } from "@react-navigation/native";
-import { getStore } from "../libraries/store";
-import get from "../libraries/get";
-import moment from "moment";
-export default function Home({ navigation }) {
-   
+import {useCallback, useState} from 'react';
+import {useFocusEffect} from '@react-navigation/native';
+import {getStore} from '../libraries/store';
+import get from '../libraries/get';
+import moment from 'moment';
+import {StyleSheet, View} from 'react-native';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryTheme,
+  VictoryLine,
+  VictoryPie,
+} from 'victory-native';
 
-    return (
-        <Box bg="#fff" h="100%" padding="24px">
-            <HStack alignItems="center">
-                <Button rounded="50" padding="0" onPress={() => navigation.goBack()} variant="ghost">
-                    <ChevronLeftIcon ></ChevronLeftIcon>
-                </Button>
-                <Text fontSize={'20px'} marginLeft="24px">Analytics</Text>
-            </HStack>
-            
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000},
+];
 
-           
-
-        </Box>
-    )
+export default function Analytics({navigation}) {
+  return (
+    <ScrollView>
+      <Box bg="#fff" h="100%" padding="24px">
+        <HStack alignItems="center">
+          <Button
+            rounded="50"
+            padding="0"
+            onPress={() => navigation.goBack()}
+            variant="ghost">
+            <ChevronLeftIcon></ChevronLeftIcon>
+          </Button>
+          <Text fontSize={'20px'} marginLeft="24px">
+            Analytics
+          </Text>
+        </HStack>
+        <View style={styles.container}>
+          <VictoryChart width={350} theme={VictoryTheme.material}>
+            <VictoryBar data={data} x="quarter" y="earnings" />
+          </VictoryChart>
+          <VictoryChart>
+            <VictoryLine
+              style={{
+                data: {stroke: '#c43a31'},
+                parent: {border: '1px solid #ccc'},
+              }}
+            />
+          </VictoryChart>
+          <VictoryPie
+            data={[
+              {x: 'Cats', y: 35},
+              {x: 'Dogs', y: 40},
+              {x: 'Birds', y: 55},
+            ]}
+          />
+        </View>
+      </Box>
+    </ScrollView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+});
